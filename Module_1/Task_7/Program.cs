5d0eaa9c104af074b1395d8024bb4278e92c473c
Roman <AverkievRE@yandex.ru>
2022-04-30 18:51:51 +0300

    Realizacion project Task_7 to Module_1

--- Module_1/Task_7/Program.cs
+++ Module_1/Task_7/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+
+namespace Task_7
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите начало диапозон");
+
+            string? str = Console.ReadLine();
+            int startRange;
+            int.TryParse(str, out startRange);
+
+            if(startRange == 0 && str != "0")
+            {
+                Console.WriteLine("Не коректный ввод началного диапозона");
+                return;
+            }
+
+            Console.WriteLine("введите конец диапозона");
+
+            str = Console.ReadLine();
+            int endRange;
+            int.TryParse(str, out endRange);
+
+            if(endRange == 0 && str != "0")
+            {
+                Console.WriteLine("Не коректный ввод конца диапозона");
+                return;
+            }
+
+            if(startRange < endRange)
+            {
+                for(int i = startRange; i < endRange; i++)
+                {
+                    if(i % 2 == 0)
+                    {
+                        Console.Write(i + "\t");
+                    }
+                }
+            }
+            else if(startRange > endRange)
+            {
+                for(int i = endRange; i < startRange; i++)
+                {
+                    if(i % 2 == 0)
+                    {
+                        Console.Write(i + "\t");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Началный диапозон и конечный диапозон одинаковые");
+            }
+        }
+    }
+}
