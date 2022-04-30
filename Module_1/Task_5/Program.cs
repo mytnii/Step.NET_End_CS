7109de00dafc24fa5d56625aa8c392b392e85c05
Roman <AverkievRE@yandex.ru>
2022-04-30 15:50:03 +0300

    Realizacion project  Task_5 to Module_1

--- Module_1/Task_5/Program.cs
+++ Module_1/Task_5/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+
+namespace Task_5
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите дату в формате дд.мм.гггг");
+            string? str = Console.ReadLine();
+            DateTime date;
+            DateTime.TryParse(str, out date);
+
+            if(str != null)
+            {
+            if(str.Length != 10)
+            {
+                    Console.WriteLine("Неправильный формат ввода");
+                    return;
+            }
+            string strDate = date.ToString();
+                for(int i = 0; i < 10; i++)
+                {
+                    if(str[i] != strDate[i])
+                    {
+                        Console.WriteLine("некоректный ввод");
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine(date);
+
+            if(date.Month == 12 || date.Month == 1 || date.Month == 2)
+            {
+                Console.WriteLine($"Winter {date.DayOfWeek}");
+            }
+            else if(date.Month >= 3 && date.Month <= 5)
+            {
+                Console.WriteLine($"Spring {date.DayOfWeek}");
+            }
+            else if(date.Month >= 6 && date.Month <= 8)
+            {
+                Console.WriteLine($"Summer {date.DayOfWeek}");
+            }
+            else
+            {
+                Console.WriteLine($"Autumn {date.DayOfWeek}");  
+            }
+        }
+    }
+}
