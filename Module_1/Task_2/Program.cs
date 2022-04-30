370314fe9bcd0e54e1932fc8673ac2f7d371439e
Roman <AverkievRE@yandex.ru>
2022-04-30 11:22:24 +0300

    Realizacion project Task_2 to Module_1

--- Module_1/Task_2/Program.cs
+++ Module_1/Task_2/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+ Пользователь вводит с клавиатуры два числа. Первое
+число — это значение, второе число процент, который
+необходимо посчитать. Например, мы ввели с клавиатуры
+90 и 10. Требуется вывести на экран 10 процентов от 90.
+Результат: 9.
+ */
+
+using System;
+
+namespace Task_2
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение");
+            double value;
+            double.TryParse(Console.ReadLine(), out value);
+
+            if(value <= 0)
+            {
+                Console.WriteLine("Неправильный ввод значения");
+                return;
+            }
+
+            Console.WriteLine("Введите вычисляемый процент");
+            int interest;
+            int.TryParse(Console.ReadLine(), out interest);
+
+            if(interest <= 0)
+            {
+                Console.WriteLine("Неправильный ввод процента");
+                return;
+            }
+
+            Console.WriteLine($"{interest} процентов от {value}: {value / 100 * interest}");
+        }
+    }
+}
