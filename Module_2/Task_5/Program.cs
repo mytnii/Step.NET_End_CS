67ef2ae9418533162df1e9a14bd04fa780d7d3a4
Roman <AverkievRE@yandex.ru>
2022-05-01 16:26:35 +0300

    Realizacion project Task_5 to Module_2

--- Module_2/Task_5/Program.cs
+++ Module_2/Task_5/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+
+namespace Task_5
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите арифметическое выражение: сложение или вычитание");
+            string? str = Console.ReadLine();
+            char symbol = ' ';
+            int value = 0;
+            int value2 = 0;
+
+            if(str != null)
+            {
+                for(int i = 0; i < str.Length; i++)
+                {
+                    if(!char.IsDigit(str[i]) && str[i] != '+' && str[i] != '-')
+                    {
+                        Console.WriteLine("неправельный ввод арифметического выражения");
+                        return;
+                    }
+                }
+                for(int i = 0; i < str.Length; i++)
+                {
+                    if(str[i] == '+' || str[i] == '-')
+                    {
+                        symbol = str[i];
+                    }
+                }
+                string[] number = str.Split(new char[] {symbol});
+                Console.WriteLine(number[0]);
+                Console.WriteLine(number[1]);
+
+                int.TryParse(number[0], out value);
+                int.TryParse(number[1], out value2);
+
+                if(symbol == '+')
+                {
+                    Console.WriteLine($"{value} {symbol} {value2} =  {value + value2}");
+                }
+                else
+                {
+                    Console.WriteLine($"{value} {symbol} {value2} =  {value - value2}");
+                }
+
+
+
+            }
+        }
+    }
+}
